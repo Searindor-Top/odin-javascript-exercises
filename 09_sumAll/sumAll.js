cdfeeb3c700274fdc82f1c942b3cccad780e5400
Daniel Serantes <221082330+Searindor-Top@users.noreply.github.com>
2025-09-30 14:17:53 +0200

    Add solution to 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if( (num1 < 0) || (num2 < 0) || !Number.isInteger(num1) || !Number.isInteger(num2)){
+        return 'ERROR';
+    }
+    maxNum = Math.max(num1, num2);
+    lowNum = Math.min(num1, num2);
 
+    outputSum = 0;
+    for(i = lowNum; i <= maxNum; i++){
+        outputSum += i;
+    }
+    return outputSum;
 };
 
 // Do not edit below this line
