d73f892018a872dc9fa5e206b5dc2bc69670ddd8
Daniel Serantes <221082330+Searindor-Top@users.noreply.github.com>
2025-09-30 14:34:00 +0200

    Add solution to 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenNumber) {
+  return Math.round(((fahrenNumber - 32) / 1.8)*10)/10; // round(num) * 10 / 10 => redondea a 1 decimal
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsNumber) {
+  return Math.round((celsNumber * 1.8 + 32)*10)/10;
 };
 
 // Do not edit below this line
