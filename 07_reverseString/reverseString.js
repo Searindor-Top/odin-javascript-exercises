734af11f15b6b409b90c4a235cce4c5a6b8b151a
Daniel Serantes <221082330+Searindor-Top@users.noreply.github.com>
2025-09-30 13:09:08 +0200

    Add solution to 07

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let outputReverseString = '';
+    explodedString = string.split('');
+    for (i = explodedString.length ; i > 0; i--){
+        outputReverseString = outputReverseString + explodedString[i-1];
+    }
+    console.log(outputReverseString);
+    return outputReverseString;
 };
 
 // Do not edit below this line
