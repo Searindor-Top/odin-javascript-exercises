7469149b482a19beaf5baea1221c0e3a3c8c7030
Daniel Serantes <221082330+Searindor-Top@users.noreply.github.com>
2025-09-30 13:47:40 +0200

    Add solution to 08

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, elementToRemove, elementToRemove2 = '', elementToRemove3 = '', elementToRemove4 = '') {
+    let outputArray = array.slice();
+
+    outputArray = outputArray.filter(element => element !== elementToRemove);
+    if(elementToRemove2 !== ''){
+        outputArray = outputArray.filter(element => element !== elementToRemove2);
+    }
+    if(elementToRemove3 !== ''){
+        outputArray = outputArray.filter(element => element !== elementToRemove3);
+    }
+    if(elementToRemove4 !== ''){
+        outputArray = outputArray.filter(element => element !== elementToRemove4);
+    }
+    
+    return outputArray;
 };
 
 // Do not edit below this line
