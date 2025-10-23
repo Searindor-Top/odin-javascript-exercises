ef3756118bda1b066284c58105758f936e1469b9
Daniel Serantes <221082330+Searindor-Top@users.noreply.github.com>
2025-10-23 12:22:04 +0200

    Add solution to 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b
 };
 
-const sum = function() {
-	
+const sum = function( inputSum ) {
+  let initialValue = 0;
+  return inputSum.reduce((accum, element) => accum + element, initialValue);
 };
 
-const multiply = function() {
-
+const multiply = function( inputMult ) {
+  let initialValue = 1;
+  return inputMult.reduce((accum, element) => accum * element, initialValue);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let accum = 1
+	for(i = 1; i <= a; i++){
+    accum = accum * i
+  }
+  return accum;
 };
 
 // Do not edit below this line
