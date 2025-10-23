43781f79d2ef57cdeabe30e7953a5980bcb67b4e
Daniel Serantes <221082330+Searindor-Top@users.noreply.github.com>
2025-10-23 13:38:30 +0200

    Add solution to 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function( inputJSON ) {
+    processedJSON = inputJSON
+    processedJSON.forEach(function(element){
+        if(element.yearOfDeath === undefined){
+            element.yearOfDeath = new Date().getFullYear();
+        }
+    });
+    sortByAgeAsc(processedJSON)
+    function sortByAgeAsc(arr) {
+        arr.sort((a, b) => (a.yearOfDeath - a.yearOfBirth) - (b.yearOfDeath - b.yearOfBirth));
+    }
+    return(processedJSON[processedJSON.length-1]);
 };
 
 // Do not edit below this line
