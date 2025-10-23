e2c8a80bad451af5758cf21a034ab380b3084e7b
Daniel Serantes <221082330+Searindor-Top@users.noreply.github.com>
2025-10-23 13:09:21 +0200

    Add solution to 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function( inputNumber ) {
+    inputNumber = +inputNumber;
+    if (inputNumber === 0){
+        return 0;
+    } else if (inputNumber === 1){
+        return 1;
+    } else if (inputNumber === 2){
+        return 1;
+    } else if (inputNumber < 0) {
+        return "OOPS";
+    } else {
+        accum = [1, 1];
+        for(i = 2; i < inputNumber; i++){
+            accum.push(accum[i-1] + accum[i-2]);
+        }
+        return (accum[accum.length-1]);
+    }
 };
 
 // Do not edit below this line
