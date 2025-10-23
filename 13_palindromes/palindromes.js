221d7a01927dfb089dbf51f97f97232dfdf41861
Daniel Serantes <221082330+Searindor-Top@users.noreply.github.com>
2025-10-23 12:49:26 +0200

    Add solution to 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,26 @@
-const palindromes = function () {
+const palindromes = function ( inputString ) {
+    inputString = inputString.toLowerCase();
+    let processedString = "";
+    for(i = 0; i < inputString.length; i++){
+        if(inputString[i].match(/^[a-z0-9]+$/i) !== null){
+            processedString = processedString + inputString[i];
+        }
+    }
 
+    let processedReverseString = '';
+    explodedString = processedString.split('');
+    for (i = explodedString.length ; i > 0; i--){
+        processedReverseString = processedReverseString + explodedString[i-1];
+    }
+
+    if( processedString === processedReverseString){
+        return true;
+    } else {
+        return false;
+    }
 };
+
+palindromes("miau, miao, miai");
 
 // Do not edit below this line
 module.exports = palindromes;
